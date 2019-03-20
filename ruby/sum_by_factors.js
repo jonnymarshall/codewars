683bb734ebe6ed07b3edfd65cd4c9f7c2b382d2d
Jonny Marshall <jonnymarshall5@gmail.com>
2019-03-20 16:34:11 +0700

    TDD set up for challenge

---
+++ ruby/sum_by_factors.js
@@ -0,0 +1,10 @@
+function sumOfDivided(lst) {
+  //your code
+  return [[2, 12]];
+}
+
+sumOfDivided([12, 15])
+// [ [2, 12], [3, 27], [5, 15] ];
+
+sumOfDivided([15,21,24,30,45])
+// [ [2, 54], [3, 135], [5, 90], [7, 21] ];
