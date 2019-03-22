6cdcf1264018d2a1969731c93c5023000afc943b
Jonny Marshall <jonnymarshall5@gmail.com>
2019-03-22 20:37:25 +0700

    cannot do the maths for this challenge. code ok. maths I have no idea. SKIP

--- ruby/sum_by_factors.js
+++ ruby/sum_by_factors.js
@@ -1,12 +1,14 @@
 function sumOfDivided(lst) {
   //your code
-  return [[2, 12]];
-  let primeSumArray = [];
-  return primeSumArray
+  lst.forEach((number) => {
+    // if (number) { do something; }
+    console.log(number);
+  });
 }
 
-sumOfDivided([12, 15])
+sumOfDivided([12, 15]);
+
 // [ [2, 12], [3, 27], [5, 15] ];
 
-sumOfDivided([15,21,24,30,45])
+sumOfDivided([15,21,24,30,45]);
 // [ [2, 54], [3, 135], [5, 90], [7, 21] ];
