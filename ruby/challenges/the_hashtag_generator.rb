9163a40889e879e54e6e3e86c0c172ad592a33b0
Jonny Marshall <jonnymarshall5@gmail.com>
2019-12-19 22:51:09 +1030

    unit/test working, but need a way to easily take codewars unit tests and create a new class method from each for testing

--- ruby/challenges/the_hashtag_generator.rb
+++ ruby/challenges/the_hashtag_generator.rb
@@ -1,18 +1,8 @@
-def generateHashtag(str)
-  "##{str.capitalize}"
-  # All words must have their first letter capitalized.
-  # If the final result is longer than 140 chars it must return false.
-  # If the input or the result is an empty string it must return false.
+class HashtagGenerator
+  def generateHashtag(str)
+    "##{str.capitalize}"
+    # All words must have their first letter capitalized.
+    # If the final result is longer than 140 chars it must return false.
+    # If the input or the result is an empty string it must return false.
+  end
 end
-
-
-Test.assert_equals(generateHashtag(""), false, "Expected an empty string to return false")
-Test.assert_equals(generateHashtag(" " * 200), false, "Still an empty string")
-Test.assert_equals(generateHashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning.")
-Test.assert_equals(generateHashtag("Codewars"), "#Codewars", "Should handle a single word.")
-Test.assert_equals(generateHashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces.")
-Test.assert_equals(generateHashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words.")
-Test.assert_equals(generateHashtag("code" + " " * 140 + "wars"), "#CodeWars")
-Test.assert_equals(generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars.")
-Test.assert_equals(generateHashtag("a" * 139), "#A" + "a" * 138, "Should work")
-Test.assert_equals(generateHashtag("a" * 140), false, "Too long")
--- ruby/spec/example_spec.rb
+++ ruby/spec/example_spec.rb
@@ -1,5 +1,36 @@
-describe 'ExampleSpec' do
-  it 'is true' do
-    expect(true).to be true
+require_relative "../challenges/the_hashtag_generator"
+require "test/unit"
+
+class ChallengeTester < Test::Unit::TestCase
+  def test_generate_hashtag
+    a = HashtagGenerator.new
+
+    unit_tests = [
+      assert_equal(a.generateHashtag(""), false, "Expected an empty string to return false"),
+      assert_equal(a.generateHashtag(" " * 200), false, "Still an empty string"),
+      assert_equal(a.generateHashtag("Do We have A Hashtag"), "#DoWeHaveAHashtag", "Expected a Hashtag (#) at the beginning."),
+      assert_equal(a.generateHashtag("Codewars"), "#Codewars", "Should handle a single word."),
+      assert_equal(a.generateHashtag("Codewars Is Nice"), "#CodewarsIsNice", "Should remove spaces."),
+      assert_equal(a.generateHashtag("Codewars is nice"), "#CodewarsIsNice", "Should capitalize first letters of words."),
+      assert_equal(a.generateHashtag("code" + " " * 140 + "wars"), "#CodeWars"),
+      assert_equal(a.generateHashtag("Looooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooong Cat"), false, "Should return false if the final word is longer than 140 chars."),
+      assert_equal(a.generateHashtag("a" * 139), "#A" + "a" * 138, "Should work"),
+      assert_equal(a.generateHashtag("a" * 140), false, "Too long")
+    ]
+
+
+    unit_tests.each do |unit_test|
+      unit_test.run_method
+    end
+  end
+end
+
+class MethodCreator(unit_test)
+  def initialize
+    unit_test = unit_test
+  end
+
+  def unit_test_to_method
+    unit_test = new method
   end
 end
