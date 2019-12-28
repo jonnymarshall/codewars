724d3c8e7451b12d9c07575fd31c3d03d2bd1147
Jonny Marshall <jonnymarshall5@gmail.com>
2019-12-28 22:40:55 +0800

    first refactor

--- ruby/challenges/tic-tac-toe_checker.rb
+++ ruby/challenges/tic-tac-toe_checker.rb
@@ -1,13 +1,11 @@
 require 'byebug'
 
-def is_solved(board, tracker = {xwin: false, owin: false, ccount: 0, rcount: 0, spaces_left: false}, columns = Array.new(3){Array.new(3)})
+def is_solved(board, tracker = {xwin: false, owin: false, spaces_left: false})
+  columns = board.transpose
   board.each do |row|
     tracker[:xwin] = true if row[0] == 1 && row[1] == 1 && row[2] == 1
     tracker[:owin] = true if row[0] == 2 && row[1] == 2 && row[2] == 2
     tracker[:spaces_left] = true if row[0] == 0 || row[1] == 0 || row[2] == 0
-    create_columns(tracker, row, columns)
-    tracker[:rcount] = 0
-    tracker[:ccount] += 1
   end
 
   columns.each do |col|
@@ -16,8 +14,6 @@ def is_solved(board, tracker = {xwin: false, owin: false, ccount: 0, rcount: 0, 
   end
 
   diag_checker(board, tracker)
-
-  p tracker
 
   if tracker[:xwin]
     # X won
@@ -31,13 +27,6 @@ def is_solved(board, tracker = {xwin: false, owin: false, ccount: 0, rcount: 0, 
   else
     # it's a draw
     return 0
-  end
-end
-
-def create_columns(tracker, row, columns)
-  row.each do |slot|
-    columns[tracker[:rcount]][tracker[:ccount]] = slot
-    tracker[:rcount] += 1
   end
 end
 
